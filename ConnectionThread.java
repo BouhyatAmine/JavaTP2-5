3e6f75193bd2913acad28e82ce781e0ea2a63349
amine <bouhyat.amine@gmail.com>
2023-12-02 17:30:32 +0100

    java project

--- ConnectionThread.java
+++ ConnectionThread.java
@@ -7,6 +7,14 @@ public class ConnectionThread extends Thread {
     private final TCPMultiServer server;
     private final String clientAddress;
 
+    /**
+     * Constructs a ConnectionThread with the specified client socket, server, and client address.
+     *
+     * @param clientSocket  the Socket representing the connected client.
+     * @param server        the TCPMultiServer instance managing the connections.
+     * @param clientAddress the IP address of the connected client.
+     * @throws IOException if there is an error initializing input/output streams.
+     */
     public ConnectionThread(Socket clientSocket, TCPMultiServer server, String clientAddress) throws IOException {
         this.clientInputStream = clientSocket.getInputStream();
         this.clientWriter = new PrintWriter(clientSocket.getOutputStream(), true);
@@ -14,20 +22,31 @@ public class ConnectionThread extends Thread {
         this.clientAddress = clientAddress;
     }
 
+    /**
+     * Runs the thread, continuously reading messages from the connected client and broadcasting them to other clients.
+     *
+     * @throws RuntimeException if there is an error reading client messages or broadcasting messages.
+     */
     @Override
     public void run() {
         try (BufferedReader inputReader = new BufferedReader(new InputStreamReader(clientInputStream))) {
             String message;
             while ((message = inputReader.readLine()) != null) {
                 System.out.println("Client message from " + clientAddress + ": " + message);
-                server.broadcast(message);
+                server.broadcast("Client message from " + getName() + " " +message);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    /**
+     * Sends a message to the connected client.
+     *
+     * @param message the message to be sent.
+     */
     public void sendMessage(String message) {
         clientWriter.println(message);
     }
 }
+
--- TCPMultiServer.java
+++ TCPMultiServer.java
@@ -18,14 +18,18 @@ public class TCPMultiServer {
         this(DEFAULT_PORT);
     }
 
+    /**
+     * Launches the TCP multiserver, setting up a ServerSocket to listen on the specified port.
+     * Continuously accepts incoming client connections and creates a new ConnectionThread for each client.
+     *
+     * @throws RuntimeException if there is an error initializing the ServerSocket or handling a client connection.
+     */
     public void launch() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server is running. Waiting for clients...");
-
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Client connected from " + clientSocket.getInetAddress());
-
                 ConnectionThread connectionThread = new ConnectionThread(clientSocket, this, clientSocket.getInetAddress().toString());
                 clientThreads.add(connectionThread);
                 connectionThread.start();
@@ -35,15 +39,38 @@ public class TCPMultiServer {
         }
     }
 
+    /**
+     * Broadcasts a message to all connected clients.
+     *
+     * @param message the message to broadcast.
+     */
     public void broadcast(String message) {
         for (ConnectionThread thread : clientThreads) {
             thread.sendMessage(message);
         }
     }
 
-
+    /**
+     * The main entry point for the TCPMultiServer application.
+     * Creates and launches a TCP multiserver using the specified port number.
+     *
+     * @param args command-line arguments containing the server port.
+     *             Usage: java TCPMultiServer <server_port>
+     */
     public static void main(String[] args) {
-        TCPMultiServer multiServer = new TCPMultiServer(Integer.parseInt(args[0]));
-        multiServer.launch();
+        if (args.length != 1) {
+            System.err.println("Usage: java TCPMultiServer <server_port>");
+            System.exit(1);
+        }
+
+        try {
+            int serverPort = Integer.parseInt(args[0]);
+            TCPMultiServer multiServer = new TCPMultiServer(serverPort);
+            multiServer.launch();
+        } catch (NumberFormatException e) {
+            System.err.println("Error: Invalid port number.");
+            System.exit(1);
+        }
     }
 }
+
