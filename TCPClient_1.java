8be7ccc17a5129fcf5267a891dabdad3b08bf035
amine <bouhyat.amine@gmail.com>
2023-12-02 17:19:25 +0100

    java project

--- TCPClient_1.java
+++ TCPClient_1.java
@@ -3,28 +3,22 @@ import java.net.InetAddress;
 import java.net.Socket;
 
 public class TCPClient_1 {
+    /**
+     * Connects to a TCP server at the specified address and port, sending and receiving messages.
+     *
+     * @param address the InetAddress of the TCP server.
+     * @param port    the port number of the TCP server.
+     * @throws IOException if there is an error establishing a connection or sending/receiving messages.
+     */
     public void send(InetAddress address, int port) throws IOException {
-        try (Socket socket = new Socket(address, port)) {
-            System.out.println("Connected to server.");
+        try (Socket socket = new Socket(address, port);
+             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+             InputStream in = socket.getInputStream();
+             BufferedReader serverReader = new BufferedReader(new InputStreamReader(in));
+             OutputStream out = socket.getOutputStream();
+             PrintWriter writer = new PrintWriter(out, true)) {
 
-            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-            InputStream in = socket.getInputStream();
-            BufferedReader serverReader = new BufferedReader(new InputStreamReader(in));
-
-            OutputStream out = socket.getOutputStream();
-            PrintWriter writer = new PrintWriter(out, true);
-
-            Thread serverReaderThread = new Thread(() -> {
-                try {
-                    String response;
-                    while ((response = serverReader.readLine()) != null) {
-                        System.out.println("\nServer: " + response);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-            serverReaderThread.start();
+            startServerReaderThread(serverReader);
 
             String message;
             while (true) {
@@ -38,11 +32,55 @@ public class TCPClient_1 {
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        String serverAddress = args[0];
-        int serverPort = Integer.parseInt(args[1]);
+    /**
+     * Starts a separate thread to continuously read and print messages from the server.
+     *
+     * @param serverReader the BufferedReader for reading messages from the server.
+     */
+    private void startServerReaderThread(BufferedReader serverReader) {
+        Thread serverReaderThread = new Thread(() -> {
+            try {
+                String response;
+                while ((response = serverReader.readLine()) != null) {
+                    System.out.println("\nServer: " + response);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        serverReaderThread.start();
+    }
 
-        TCPClient_1 client = new TCPClient_1();
-        client.send(InetAddress.getByName(serverAddress), serverPort);
+    /**
+     * The main entry point for the TCPClient_1 application.
+     * Connects to a TCP server using the specified server address and port.
+     *
+     * @param args command-line arguments containing the server address and port.
+     *             Usage: java TCPClient_1 <server_address> <server_port>
+     * @throws IOException if there is an error establishing a connection or sending/receiving messages.
+     */
+    public static void main(String[] args) throws IOException {
+        // Check if the correct number of command-line arguments is provided
+        if (args.length != 2) {
+            System.err.println("Usage: java TCPClient_1 <server_address> <server_port>");
+            System.exit(1);
+        }
+
+        try {
+            // Parse command-line arguments
+            String serverAddress = args[0];
+            int serverPort = Integer.parseInt(args[1]);
+
+            // Create and launch the TCP client
+            TCPClient_1 client = new TCPClient_1();
+            client.send(InetAddress.getByName(serverAddress), serverPort);
+
+        } catch (NumberFormatException e) {
+            // Handle the case where the provided port number is not a valid integer
+            System.err.println("Error: Invalid port number.");
+            System.exit(1);
+        }
     }
 }
+
+
--- TCPServer.java
+++ TCPServer.java
@@ -13,41 +13,100 @@ public class TCPServer {
         this(DEFAULT_PORT);
     }
 
+    /**
+     * Launches the TCP server, setting up a ServerSocket to listen on the specified port.
+     * The server continuously accepts incoming client connections and delegates handling each connection
+     * to the {@link #handleClientConnection(Socket)} method.
+     *
+     * @throws RuntimeException if there is an error initializing the ServerSocket or handling a client connection.
+     */
     public void launch() {
-        try {
-            ServerSocket serverSocket = new ServerSocket(port);
+        try (ServerSocket serverSocket = new ServerSocket(port)){
             System.out.println("Server On. Waiting for client...");
 
-            while(true){
-                Socket socket = serverSocket.accept();
-                System.out.println("Client connected");
-
-                InputStream in = socket.getInputStream();
-                BufferedReader inputReader = new BufferedReader(new InputStreamReader(in));
-
-                OutputStream out = socket.getOutputStream();
-                PrintWriter writer = new PrintWriter(out, true);
-
-                String message = "";
-
-                while (!socket.isClosed()) {
-                    message = inputReader.readLine();
-                    if (message != null){
-                        System.out.println("Message  received: " + message);
-                        writer.println("Message sent back from the server: " + message);
-                    } else {
-                        System.out.println("Connection closed");
-                        socket.close();
-                    }
-                }
+            while (true) {
+                handleClientConnection(serverSocket.accept());
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        TCPServer myTCPserver = new TCPServer(Integer.parseInt(args[0]));
-        myTCPserver.launch();
+    /**
+     * Handles a client connection by setting up input and output streams, and continuously
+     * reads messages from the client. Received messages are printed to the console, and a response
+     * is sent back to the client.
+     *
+     * @param socket the Socket representing the client connection.
+     * @throws IOException if there is an error setting up input/output streams or handling the client connection.
+     */
+    private void handleClientConnection(Socket socket) throws IOException {
+        System.out.println("Client connected");
+
+        try (InputStream in = socket.getInputStream();
+             BufferedReader inputReader = new BufferedReader(new InputStreamReader(in));
+             OutputStream out = socket.getOutputStream();
+             PrintWriter writer = new PrintWriter(out, true)) {
+
+            processClientMessages(socket, inputReader, writer);
+        }
     }
+
+    /**
+     * Processes messages received from the client. Each received message is printed to the console,
+     * and a response is sent back to the client.
+     *
+     * @param socket       the Socket representing the client connection.
+     * @param inputReader  the BufferedReader for reading messages from the client.
+     * @param writer       the PrintWriter for sending responses to the client.
+     * @throws IOException if there is an error processing client messages or sending responses.
+     */
+    private void processClientMessages(Socket socket, BufferedReader inputReader, PrintWriter writer) throws IOException {
+        String message;
+        while (!socket.isClosed()) {
+            message = inputReader.readLine();
+            if (message != null) {
+                System.out.println("Message received: " + message);
+                sendResponse(writer, "Message sent back from the server: " + message);
+            } else {
+                System.out.println("Connection closed");
+                socket.close();
+            }
+        }
+    }
+
+    /**
+     * Sends a response message to the client using the provided PrintWriter.
+     *
+     * @param writer  the PrintWriter for sending responses to the client.
+     * @param message the response message to be sent.
+     */
+    private void sendResponse(PrintWriter writer, String message) {
+        writer.println(message);
+    }
+
+    /**
+     * The main entry point for the TCPServer application.
+     * Creates and launches a TCP server using the specified port number.
+     *
+     * @param args command-line arguments containing the server port.
+     *             Usage: java TCPServer <server_port>
+     * @throws IOException if there is an error initializing or launching the TCP server.
+     */
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: java TCPServer <server_port>");
+            System.exit(1);
+        }
+
+        try {
+            int serverPort = Integer.parseInt(args[0]);
+            TCPServer myTCPserver = new TCPServer(serverPort);
+            myTCPserver.launch();
+        } catch (NumberFormatException e) {
+            System.err.println("Error: Invalid port number.");
+            System.exit(1);
+        }
+    }
+
 }
